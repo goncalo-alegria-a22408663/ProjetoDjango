052da7b8840f1c5832d974a2212e176aaca99264
Goncalo Alegria <a22408663@alunos.ulht.pt>
2026-04-11 20:52:07 +0000

    Implementação do modelo Docente com choices de habilitação e regime

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Licenciatura
+from .models import Licenciatura, Docente
 
 
 @admin.register(Licenciatura)
@@ -7,3 +7,10 @@ class LicenciaturaAdmin(admin.ModelAdmin):
     list_display = ('sigla', 'nome', 'instituicao', 'duracao_anos', 'total_ects')
     search_fields = ('nome', 'sigla', 'instituicao')
     list_filter = ('instituicao', 'duracao_anos')
+
+
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'habilitacao', 'regime_contrato')
+    search_fields = ('nome',)
+    list_filter = ('habilitacao', 'regime_contrato')
--- portfolio/models.py
+++ portfolio/models.py
@@ -13,3 +13,26 @@ class Licenciatura(models.Model):
 
     def __str__(self):
         return self.sigla
+
+class Docente(models.Model):
+    HABILITACAO_CHOICES = [
+        ('ALU_MES', 'Aluno de Mestrado'),
+        ('MES', 'Mestrado'),
+        ('ALU_DOUT', 'Aluno de Doutoramento'),
+        ('DOUT', 'Doutoramento'),
+        ('AGR', 'Agregação'),
+    ]
+
+    REGIME_CHOICES = [
+        ('TI', 'Tempo Integral'),
+        ('TP', 'Tempo Parcial'),
+    ]
+
+    nome = models.CharField(max_length=200)
+    link_pagina_pessoal = models.URLField(blank=True)
+    foto = models.ImageField(upload_to='docentes/', blank=True)
+    habilitacao = models.CharField(max_length=10, choices=HABILITACAO_CHOICES)
+    regime_contrato = models.CharField(max_length=5, choices=REGIME_CHOICES)
+
+    def __str__(self):
+        return self.nome
