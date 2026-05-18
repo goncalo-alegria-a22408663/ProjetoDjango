dc1f91cba49a7404cebefa17d4418b2cf763aa4b
Goncalo Alegria <a22408663@alunos.ulht.pt>
2026-05-18 19:12:35 +0000

    Configurar base de dados PostgreSQL com DATABASE_URL

--- project/settings.py
+++ project/settings.py
@@ -3,7 +3,7 @@ Django settings for project.
 """
 
 from pathlib import Path
-
+import dj_database_url
 import environ
 
 
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": env.db(
+    "default": dj_database_url.config(
         "DATABASE_URL",
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
     )
